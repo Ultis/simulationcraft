Ultis/simulationcraft

commit f12d779dd48138a5f2d5e06c95c9e79d3554764d

Rewrite of HTTP caching to guarantee 1. Every request for a URL in a single simulation run gets the same value, 2. Any resource is downloaded at most once in a single simulation run.
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@9293 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_sim.cpp
@@ -145,9 +145,9 @@ static bool parse_player( sim_t*             sim,
   if ( name == "player" )
   {
     std::string player_name = value;
-    std::string player_options = "";
+    std::string player_options;
 
-    std::string::size_type cut_pt = value.find_first_of( "," );
+    std::string::size_type cut_pt = value.find_first_of( ',' );
 
     if ( cut_pt != value.npos )
     {
@@ -159,47 +159,46 @@ static bool parse_player( sim_t*             sim,
     std::string region = sim -> default_region_str;
     std::string server = sim -> default_server_str;
     std::string talents = "active";
-    int cache=0;
+    int use_cache=0;
 
     option_t options[] =
     {
-      { "wowhead", OPT_STRING, &wowhead  },
-      { "region",  OPT_STRING, &region   },
-      { "server",  OPT_STRING, &server   },
-      { "talents", OPT_STRING, &talents  },
-      { "cache",   OPT_BOOL,   &cache    },
+      { "wowhead", OPT_STRING, &wowhead   },
+      { "region",  OPT_STRING, &region    },
+      { "server",  OPT_STRING, &server    },
+      { "talents", OPT_STRING, &talents   },
+      { "cache",   OPT_BOOL,   &use_cache },
       { NULL, OPT_UNKNOWN, NULL }
     };
 
     option_t::parse( sim, "player", options, player_options );
 
     sim -> input_is_utf8 = utf8::is_valid( player_name.begin(), player_name.end() ) && utf8::is_valid( server.begin(), server.end() );
+    cache::behavior_t caching = use_cache ? cache::ANY : cache::behavior();
 
     if ( wowhead.empty() )
     {
       if ( true )
-        sim -> active_player = bcp_api::download_player( sim, region, server, player_name, "active" );
+        sim -> active_player = bcp_api::download_player( sim, region, server, player_name, "active", caching );
       else
       {
         if ( region == "cn" )
         {
-          sim -> active_player = armory_t::download_player( sim, region, server, player_name, "active" );
+          sim -> active_player = armory_t::download_player( sim, region, server, player_name, "active", caching );
         }
         else
         {
-          sim -> active_player = battle_net_t::download_player( sim, region, server, player_name, "active" );
+          sim -> active_player = battle_net_t::download_player( sim, region, server, player_name, "active", caching );
         }
       }
     }
     else
     {
-      sim -> active_player = wowhead_t::download_player( sim, wowhead, ( talents == "active" ) );
+      sim -> active_player = wowhead_t::download_player( sim, wowhead, ( talents == "active" ), caching );
 
-      if ( sim -> active_player )
-        if ( player_name != sim -> active_player -> name() )
-          sim -> errorf( "Mismatch between player name '%s' and wowhead name '%s' for id '%s'\n",
-                         player_name.c_str(), sim -> active_player -> name(), wowhead.c_str() );
-
+      if ( sim -> active_player && player_name != sim -> active_player -> name() )
+        sim -> errorf( "Mismatch between player name '%s' and wowhead name '%s' for id '%s'\n",
+                       player_name.c_str(), sim -> active_player -> name(), wowhead.c_str() );
     }
   }
   else if( name == "pet" )
@@ -218,7 +217,7 @@ static bool parse_player( sim_t*             sim,
   }
   else if ( name == "copy" )
   {
-    std::string::size_type cut_pt = value.find_first_of( "," );
+    std::string::size_type cut_pt = value.find_first_of( ',' );
 
     player_t* source;
     std::string player_name;
@@ -281,6 +280,22 @@ static bool parse_proxy( sim_t*             sim,
 
 // parse_armory =============================================================
 
+static bool parse_cache( sim_t*             sim,
+                         const std::string& name,
+                         const std::string& value )
+{
+  if ( name != "cache" ) return false;
+
+  if ( value == "1" ) cache::behavior( cache::ANY );
+  else if ( value == "0" )cache::behavior( cache::CURRENT );
+  else if ( util_t::str_compare_ci( value, "only" ) ) cache::behavior( cache::ONLY );
+  else return false;
+
+  return true;
+}
+
+// parse_armory =============================================================
+
 static bool parse_armory( sim_t*             sim,
                           const std::string& name,
                           const std::string& value )
@@ -308,12 +323,13 @@ static bool parse_armory( sim_t*             sim,
         player_name.erase( 0, 1 );
         description = "inactive";
       }
-      std::vector<std::string> encoding;
-      if ( util_t::string_split( encoding, player_name, "|" ) > 1 )
+      std::string::size_type pos = player_name.find( '|' );
+      if ( pos != player_name.npos )
       {
-        player_name = encoding[ 0 ];
-        description = encoding[ 1 ];
+        description.assign( player_name, pos + 1, player_name.npos );
+        player_name.erase( pos );
       }
+
       if ( ! sim -> input_is_utf8 )
         sim -> input_is_utf8 = utf8::is_valid( player_name.begin(), player_name.end() ) && utf8::is_valid( server.begin(), server.end() );
 
@@ -336,11 +352,11 @@ static bool parse_armory( sim_t*             sim,
   else if ( name == "guild" )
   {
     std::string guild_name = value;
-    std::string guild_options = "";
+    std::string guild_options;
     std::vector<int> ranks_list;
     std::vector<std::string> ranks;
 
-    std::string::size_type cut_pt = value.find_first_of( "," );
+    std::string::size_type cut_pt = value.find_first_of( ',' );
 
     if ( cut_pt != value.npos )
     {
@@ -353,16 +369,16 @@ static bool parse_armory( sim_t*             sim,
     std::string type_str;
     std::string ranks_str;
     int max_rank = 0;
-    int cache = 0;
+    int use_cache = 0;
 
     option_t options[] =
     {
-      { "region",   OPT_STRING, &region   },
-      { "server",   OPT_STRING, &server   },
-      { "class",    OPT_STRING, &type_str },
-      { "max_rank", OPT_INT,    &max_rank },
-      { "ranks",    OPT_STRING, &ranks_str},
-      { "cache",    OPT_BOOL,   &cache    },
+      { "region",   OPT_STRING, &region    },
+      { "server",   OPT_STRING, &server    },
+      { "class",    OPT_STRING, &type_str  },
+      { "max_rank", OPT_INT,    &max_rank  },
+      { "ranks",    OPT_STRING, &ranks_str },
+      { "cache",    OPT_BOOL,   &use_cache },
       { NULL, OPT_UNKNOWN, NULL }
     };
 
@@ -384,16 +400,18 @@ static bool parse_armory( sim_t*             sim,
     int player_type = PLAYER_NONE;
     if ( ! type_str.empty() ) player_type = util_t::parse_player_type( type_str );
 
+    cache::behavior_t caching = use_cache ? cache::ANY : cache::behavior();
+
     if ( true )
-      return bcp_api::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, cache != 0 );
+      return bcp_api::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, caching );
 
     if ( region == "cn" )
     {
-      return armory_t::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, cache );
+      return armory_t::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, caching );
     }
     else
     {
-      return battle_net_t::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, cache );
+      return battle_net_t::download_guild( sim, region, server, guild_name, ranks_list, player_type, max_rank, caching );
     }
   }
 
@@ -515,9 +533,8 @@ static bool parse_bcp_api( sim_t*             sim,
     std::string::size_type pos = splits[ i ].find('|');
     if ( pos != std::string::npos )
     {
-      std::string::size_type n = splits[ i ].length() - pos - 1;
-      talents.assign( splits[ i ], pos + 1, n );
-      splits[ i ].erase( pos, n + 1 );
+      talents.assign( splits[ i ], pos + 1, std::string::npos );
+      splits[ i ].erase( pos );
     }
 
     sim -> active_player = bcp_api::download_player( sim, region, server, splits[ i ], talents );
@@ -698,7 +715,8 @@ sim_t::sim_t( sim_t* p, int index ) :
     enchant = parent -> enchant;
 
     seed = parent -> seed;
-  }
+  } else
+    cache::advance_era();
 }
 
 // sim_t::~sim_t ============================================================
@@ -2278,6 +2296,7 @@ void sim_t::create_options()
     { "rawr",                             OPT_FUNC,   ( void* ) ::parse_rawr                        },
     { "bcp",                              OPT_FUNC,   ( void* ) ::parse_bcp_api                     },
     { "http_clear_cache",                 OPT_FUNC,   ( void* ) ::http_t::clear_cache               },
+    { "cache",                            OPT_FUNC,   ( void* ) ::parse_cache                       },
     { "default_region",                   OPT_STRING, &( default_region_str                       ) },
     { "default_server",                   OPT_STRING, &( default_server_str                       ) },
     { "save_prefix",                      OPT_STRING, &( save_prefix_str                          ) },
@@ -2463,6 +2482,7 @@ int sim_t::main( int argc, char** argv )
   thread_t::init();
 
   http_t::cache_load();
+  cache::advance_era();
 
   dbc_t::init();
 

engine/sc_armory.cpp
@@ -104,10 +104,10 @@ static xml_node_t* download_character_sheet( sim_t* sim,
                                              const std::string& region,
                                              const std::string& server,
                                              const std::string& name,
-                                             int cache )
+                                             cache::behavior_t  caching )
 {
   std::string url = "http://" + region + ".wowarmory.com/character-sheet.xml?locale=en_US&r=" + server + "&n=" + name;
-  xml_node_t* node = xml_t::download( sim, url, "</characterTab>", ( cache ? 0 : -1 ), sim -> current_throttle );
+  xml_node_t* node = xml_t::get( sim, url, "</characterTab>", caching, sim -> current_throttle );
 
   if ( ! node )
   {
@@ -129,10 +129,10 @@ static xml_node_t* download_character_talents( sim_t* sim,
                                                const std::string& region,
                                                const std::string& server,
                                                const std::string& name,
-                                               int   cache )
+                                               cache::behavior_t  caching )
 {
   std::string url = "http://" + region + ".wowarmory.com/character-talents.xml?locale=en_US&r=" + server + "&n=" + name;
-  xml_node_t* node = xml_t::download( sim, url, "</talentGroup>", ( cache ? 0 : -1 ), sim -> current_throttle );
+  xml_node_t* node = xml_t::get( sim, url, "</talentGroup>", caching, sim -> current_throttle );
 
   if ( ! node )
   {
@@ -153,7 +153,7 @@ static xml_node_t* download_character_talents( sim_t* sim,
 static xml_node_t* download_item_tooltip( player_t* p,
                                           const std::string& id_str,
                                           int slot,
-                                          int cache_only )
+                                          cache::behavior_t caching )
 {
   sim_t* sim = p -> sim;
 
@@ -165,11 +165,9 @@ static xml_node_t* download_item_tooltip( player_t* p,
 
   xml_node_t* node;
 
-  if ( cache_only )
-    node = xml_t::download_cache( sim, url, p -> last_modified );
-  else
+  node = xml_t::get( sim, url, "</itemTooltip>", caching, sim -> current_throttle );
+  if ( caching != cache::ONLY )
   {
-    node = xml_t::download( sim, url, "</itemTooltip>", p -> last_modified, sim -> current_throttle );
     if ( ! node )
     {
       sim -> current_throttle = sim -> current_throttle > 20 ? sim -> current_throttle : 20 ;
@@ -189,18 +187,16 @@ static xml_node_t* download_item_tooltip( player_t* p,
 
 static xml_node_t* download_item_tooltip( player_t* p,
                                           const std::string& id_str,
-                                          int cache_only=0 )
+                                          cache::behavior_t caching )
 {
   sim_t* sim = p -> sim;
 
   std::string url = "http://" + p -> region_str + ".wowarmory.com/item-tooltip.xml?i=" + id_str;
   xml_node_t* node;
 
-  if ( cache_only )
-    node = xml_t::download_cache( sim, url );
-  else
+  node = xml_t::get( sim, url, "</itemTooltip>", caching, sim -> current_throttle );
+  if ( caching != cache::ONLY )
   {
-    node = xml_t::download( sim, url, "</itemTooltip>", 0, sim -> current_throttle );
     if ( ! node )
     {
       sim -> current_throttle = sim -> current_throttle > 20 ? sim -> current_throttle : 20 ;
@@ -672,11 +668,11 @@ bool armory_t::download_guild( sim_t* sim,
                                const std::vector<int>& ranks,
                                int player_filter,
                                int max_rank,
-                               int cache )
+                               cache::behavior_t caching )
 {
   std::string url = "http://" + region + ".wowarmory.com/guild-info.xml?r=" + server + "&gn=" + name;
 
-  xml_node_t* guild_info = xml_t::download( sim, url, "</members>", ( cache ? 0 : -1 ), sim -> current_throttle );
+  xml_node_t* guild_info = xml_t::get( sim, url, "</members>", caching, sim -> current_throttle );
   if ( ! guild_info )
   {
     sim -> current_throttle = sim -> current_throttle > 20 ? sim -> current_throttle : 20 ;
@@ -742,12 +738,12 @@ bool armory_t::download_guild( sim_t* sim,
       util_t::format_text( formatted_name, sim -> input_is_utf8 );
 
       sim -> errorf( "Downloading character: %s\n", formatted_name.c_str() );
-      player_t* p = armory_t::download_player( sim, region, server, character_name, "active", cache );
+      player_t* p = armory_t::download_player( sim, region, server, character_name, "active", caching );
 
       if ( ! p )
       {
         sim -> errorf( "simulationcraft: Armory failed...  Attempting to download character from wowhead.\n" );
-        p = wowhead_t::download_player( sim, region, server, character_name, 1 );
+        p = wowhead_t::download_player( sim, region, server, character_name, true, caching );
       }
       if ( ! p ) return false;
 
@@ -775,13 +771,13 @@ player_t* armory_t::download_player( sim_t* sim,
                                      const std::string& server,
                                      const std::string& name,
                                      const std::string& talents_description,
-                                     int cache )
+                                     cache::behavior_t  caching )
 {
   sim -> current_slot = 0;
   sim -> current_name = name;
 
-  xml_node_t*   sheet_xml = download_character_sheet  ( sim, region, server, name, cache );
-  xml_node_t* talents_xml = download_character_talents( sim, region, server, name, cache );
+  xml_node_t*   sheet_xml = download_character_sheet  ( sim, region, server, name, caching );
+  xml_node_t* talents_xml = download_character_talents( sim, region, server, name, caching );
 
   if ( ! sheet_xml || ! talents_xml )
   {
@@ -810,7 +806,7 @@ player_t* armory_t::download_player( sim_t* sim,
 
   if( ! talents_description.empty() && ( talents_description != "active" ) )
   {
-    name_str += "_" + talents_description;
+    name_str += '_' + talents_description;
   }
 
   player_t* p = player_t::create( sim, type_str, name_str, r );
@@ -844,8 +840,8 @@ player_t* armory_t::download_player( sim_t* sim,
     if ( xml_t::get_value(   key_str, skill_nodes[ i ], "key"   ) &&
          xml_t::get_value( value_str, skill_nodes[ i ], "value" ) )
     {
-      if ( i ) p -> professions_str += "/";
-      p -> professions_str += key_str + "=" + value_str;
+      if ( i ) p -> professions_str += '/';
+      p -> professions_str += key_str + '=' + value_str;
     }
   }
 
@@ -988,14 +984,14 @@ player_t* armory_t::download_player( sim_t* sim,
 
 bool armory_t::download_slot( item_t& item,
                               const std::string& id_str,
-                              int cache_only )
+                              cache::behavior_t  caching )
 {
   player_t* p = item.player;
 
-  xml_node_t* slot_xml = download_item_tooltip( p, id_str, item.slot, cache_only );
+  xml_node_t* slot_xml = download_item_tooltip( p, id_str, item.slot, caching );
   if ( ! slot_xml )
   {
-    if ( ! cache_only )
+    if ( caching != cache::ONLY )
       item.sim -> errorf( "Unable to download item %s from armory at slot %d for player %s\n", id_str.c_str(), item.slot, p -> name() );
     return false;
   }
@@ -1061,14 +1057,14 @@ bool armory_t::download_slot( item_t& item,
 
 bool armory_t::download_item( item_t& item,
                               const std::string& id_str,
-                              int cache_only )
+                              cache::behavior_t  caching )
 {
   player_t* p = item.player;
 
-  xml_node_t* item_xml = download_item_tooltip( p, id_str, cache_only );
+  xml_node_t* item_xml = download_item_tooltip( p, id_str, caching );
   if ( ! item_xml )
   {
-    if ( ! cache_only )
+    if ( caching != cache::ONLY )
       item.sim -> errorf( "Player %s unable to download item %s from armory at slot %s.\n", p -> name(), id_str.c_str(), item.slot_name() );
     return false;
   }

engine/sc_battle_net.cpp
@@ -12,10 +12,11 @@ namespace {  // ANONYMOUS NAMESPACE ==========================================
 static xml_node_t* download_character_sheet( sim_t* sim,
                                              const std::string& region,
                                              const std::string& server,
-                                             const std::string& name )
+                                             const std::string& name,
+                                             cache::behavior_t caching )
 {
   std::string url = "http://" + region + ".battle.net/wow/en/character/" + server + "/" + name + "/advanced";
-  xml_node_t* node = xml_t::download( sim, url, "", -1, sim -> current_throttle );
+  xml_node_t* node = xml_t::get( sim, url, std::string(), caching, sim -> current_throttle );
   if ( sim -> debug ) xml_t::print( node, sim -> output_file );
   return node;
 }
@@ -26,10 +27,11 @@ static xml_node_t* download_character_talents( sim_t* sim,
                                                const std::string& region,
                                                const std::string& server,
                                                const std::string& name,
-                                               const char* p_s )
+                                               const char* p_s,
+                                               cache::behavior_t caching )
 {
   std::string url = "http://" + region + ".battle.net/wow/en/character/" + server + "/" + name + "/talent/" + p_s;
-  xml_node_t* node = xml_t::download( sim, url, "", -1, sim -> current_throttle );
+  xml_node_t* node = xml_t::get( sim, url, std::string(), caching, sim -> current_throttle );
   if ( sim -> debug ) xml_t::print( node, sim -> output_file );
   return node;
 }
@@ -43,12 +45,12 @@ player_t* battle_net_t::download_player( sim_t* sim,
                                          const std::string& server,
                                          const std::string& name,
                                          const std::string& talents_description,
-                                         int cache )
+                                         cache::behavior_t caching )
 {
   sim -> current_slot = 0;
   sim -> current_name = name;
 
-  xml_node_t* sheet_xml   = download_character_sheet( sim, region, server, name );
+  xml_node_t* sheet_xml   = download_character_sheet( sim, region, server, name, caching );
   xml_node_t* talents_xml = 0;
 
   if ( ! sheet_xml )
@@ -67,11 +69,11 @@ player_t* battle_net_t::download_player( sim_t* sim,
 
   if ( util_t::str_compare_ci( talents_description, "primary" ) )
   {
-    talents_xml = download_character_talents( sim, region, server, name, "primary" );
+    talents_xml = download_character_talents( sim, region, server, name, "primary", caching );
   }
   else if ( util_t::str_compare_ci( talents_description, "secondary" ) )
   {
-    talents_xml = download_character_talents( sim, region, server, name, "secondary" );
+    talents_xml = download_character_talents( sim, region, server, name, "secondary", caching );
   }
 
   xml_node_t* profile_info = xml_t::get_node( sheet_xml, "div", "class", "profile-info" );
@@ -208,7 +210,7 @@ player_t* battle_net_t::download_player( sim_t* sim,
       if( ( util_t::str_compare_ci( talents_description,   "active" ) &&   active ) ||
           ( util_t::str_compare_ci( talents_description, "inactive" ) && ! active ) )
       {
-        talents_xml = download_character_talents( sim, region, server, name, ( primary ? "primary" : "secondary" ) );
+        talents_xml = download_character_talents( sim, region, server, name, ( primary ? "primary" : "secondary" ), caching );
         break;
       }
 
@@ -218,7 +220,7 @@ player_t* battle_net_t::download_player( sim_t* sim,
         armory_t::format( build_str );
         if( util_t::str_compare_ci( talents_description, build_str ) )
         {
-          talents_xml = download_character_talents( sim, region, server, name, ( primary ? "primary" : "secondary" ) );
+          talents_xml = download_character_talents( sim, region, server, name, ( primary ? "primary" : "secondary" ), caching );
           break;
         }
       }
@@ -335,7 +337,7 @@ player_t* battle_net_t::download_player( sim_t* sim,
       id_str = id_str.substr( id_str.rfind('/') + 1 );
     else
       id_str = "";
-    
+
     std::vector<std::string> tokens;
     int num_tokens = util_t::string_split( tokens, data_item_str, "&=" );
 
@@ -387,7 +389,7 @@ bool battle_net_t::download_guild( sim_t* sim,
                                    const std::vector<int>& ranks,
                                    int player_filter,
                                    int max_rank,
-                                   int cache )
+                                   cache::behavior_t caching )
 {
   std::string url_name = name;
   std::string url_server = server;
@@ -395,7 +397,7 @@ bool battle_net_t::download_guild( sim_t* sim,
   util_t::urlencode( url_server );
   std::string url = "http://" + region + ".battle.net/wow/en/guild/" + url_server + "/" + url_name + "/roster";
 
-  xml_node_t* node = xml_t::download( sim, url, "", ( cache ) ? 0 : -1, sim -> current_throttle );
+  xml_node_t* node = xml_t::get( sim, url, std::string(), caching, sim -> current_throttle );
   xml_node_t* guild_info = xml_t::get_node( xml_t::get_node( node, "div", "id", "roster" ), "tbody" );
   if ( ! guild_info ) return false;
 
@@ -468,7 +470,7 @@ bool battle_net_t::download_guild( sim_t* sim,
       std::string& character_name = character_names[ i ];
 
       sim -> errorf( "Downloading character: %s\n", character_name.c_str() );
-      player_t* p = battle_net_t::download_player( sim, region, server, character_name, "active", cache );
+      player_t* p = battle_net_t::download_player( sim, region, server, character_name, "active", caching );
 
       if ( ! p )
       {

commit 23d76afc46f55722e4b38ed34bc418ea284a10f3

Fix MSVC breakage from r9293.
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@9295 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_http.cpp
@@ -79,7 +79,7 @@ static bool parse_url( std::string&    host,
   {
     if ( *url == ':' )
     {
-      port = strtol( url + 1, const_cast<char**>( &url ), 10 );
+      port = static_cast<unsigned short>( strtol( url + 1, const_cast<char**>( &url ), 10 ) );
       break;
     }
 
@@ -200,7 +200,7 @@ static bool download( std::string& result,
 
   result.clear();
   HINTERNET hINet = InternetOpen( L"Firefox/3.0", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0 );
-  if ( !hInet )
+  if ( !hINet )
     return false;
 
   std::wstring wURL( url.length(), L' ' );

engine/sc_sim.cpp
@@ -432,11 +432,11 @@ static bool parse_wowhead( sim_t*             sim,
     if ( num_splits == 1 )
     {
       std::string player_id = splits[ 0 ];
-      int active = 1;
+      bool active = true;
       if ( player_id[ 0 ] == '!' )
       {
         player_id.erase( 0, 1 );
-        active = 0;
+        active = false;
       }
       sim -> active_player = wowhead_t::download_player( sim, player_id, active );
     }
@@ -448,11 +448,11 @@ static bool parse_wowhead( sim_t*             sim,
       for ( int i=2; i < num_splits; i++ )
       {
         std::string player_name = splits[ i ];
-        int active = 1;
+        bool active = true;
         if ( player_name[ 0 ] == '!' )
         {
           player_name.erase( 0, 1 );
-          active = 0;
+          active = false;
         }
         sim -> active_player = wowhead_t::download_player( sim, region, server, player_name, active );
         if ( ! sim -> active_player ) return false;