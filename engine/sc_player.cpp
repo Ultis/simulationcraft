Ultis/simulationcraft

commit a4e78fa37453a45be78ba7ea578b4910782a6548

Move many iteration resets to combat_begin so that reset() can be called infight. Save pet expiration event and cancel it on dismiss.
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@9930 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_player.cpp
@@ -2747,6 +2747,20 @@ void player_t::combat_begin()
     new ( sim ) vengeance_t( this );
 
   action_sequence = "";
+
+  iteration_fight_length = 0;
+  iteration_waiting_time = 0;
+  iteration_executed_foreground_actions = 0;
+  iteration_dmg = 0;
+  iteration_heal = 0;
+  iteration_dmg_taken = 0;
+  iteration_heal_taken = 0;
+
+  for ( buff_t* b = buff_list; b; b = b -> next )
+    b -> combat_begin();
+
+  for ( stats_t* s = stats_list; s; s = s -> next )
+    s -> combat_begin();
 }
 
 // player_t::combat_end =====================================================
@@ -2774,8 +2788,6 @@ void player_t::combat_end()
     pet -> combat_end();
   }
 
-  if ( sim -> debug ) log_t::output( sim, "Combat ends for player %s", name() );
-
   if ( ! is_pet() )
   {
     demise();
@@ -2800,8 +2812,12 @@ void player_t::combat_end()
   }
   compound_dmg.add( iteration_dmg );
 
+  assert( sim -> current_time > 0  );
+
   dps.add( iteration_fight_length ? iteration_dmg / iteration_fight_length : 0 );
   dpse.add( sim -> current_time ? iteration_dmg / sim -> current_time : 0 );
+
+  if ( sim -> debug ) log_t::output( sim, "Combat ends for player %s at time %.4f fight_length=%.4f", name(), sim -> current_time, iteration_fight_length );
 
   // Heal
   heal.add( iteration_heal );
@@ -2824,7 +2840,7 @@ void player_t::combat_end()
 
   for ( buff_t* b = buff_list; b; b = b -> next )
   {
-    b -> uptime_pct.add( iteration_fight_length ? 100.0 * b -> iteration_uptime_sum / iteration_fight_length : 0 );
+    b -> combat_end();
   }
 
 }
@@ -2915,14 +2931,6 @@ void player_t::reset()
 
   sleeping = 1;
   events = 0;
-
-  iteration_fight_length = 0;
-  iteration_waiting_time = 0;
-  iteration_executed_foreground_actions = 0;
-  iteration_dmg = 0;
-  iteration_heal = 0;
-  iteration_dmg_taken = 0;
-  iteration_heal_taken = 0;
 
   stats = initial_stats;
 
@@ -3180,12 +3188,13 @@ void player_t::demise()
   if ( sim -> log )
     log_t::output( sim, "%s demises.", name() );
 
-  sleeping = 1;
-  readying = 0;
 
   assert( arise_time >= 0 );
   iteration_fight_length += ( sim -> current_time - arise_time );
   arise_time = -1;
+
+  sleeping = 1;
+  readying = 0;
 
   for( buff_t* b = buff_list; b; b = b -> next )
   {

engine/sc_buff.cpp
@@ -383,7 +383,26 @@ void buff_t::init_buff_t_()
 // buff_t::~buff_t ==========================================================
 
 buff_t::~buff_t()
-{ range::dispose( stack_uptime ); }
+{
+  range::dispose( stack_uptime );
+}
+
+// buff_t::combat_begin ==========================================================
+
+void buff_t::combat_begin()
+{
+  iteration_uptime_sum = 0;
+}
+
+// buff_t::combat_end ==========================================================
+
+void buff_t::combat_end()
+{
+  if ( player )
+    uptime_pct.add( player -> iteration_fight_length ? 100.0 * iteration_uptime_sum / player -> iteration_fight_length : 0 );
+  else
+    uptime_pct.add( sim -> current_time ? 100.0 * iteration_uptime_sum / sim -> current_time : 0 );
+}
 
 // buff_t::may_react ========================================================
 
@@ -820,7 +839,6 @@ void buff_t::reset()
   expire();
   last_start = -1;
   last_trigger = -1;
-  iteration_uptime_sum = 0;
 }
 
 // buff_t::merge ============================================================

engine/sc_pet.cpp
@@ -16,6 +16,7 @@ void pet_t::init_pet_t_()
   target = owner -> target;
   level = owner -> level;
   full_name_str = owner -> name_str + '_' + name_str;
+  expiration = 0;
 
   pet_t** last = &( owner -> pet_list );
   while ( *last ) last = &( ( *last ) -> next_pet );
@@ -111,6 +112,7 @@ void pet_t::reset()
 {
   player_t::reset();
   summon_time = 0;
+  expiration = 0;
 }
 
 // pet_t::summon ============================================================
@@ -140,10 +142,11 @@ void pet_t::summon( double duration )
 
       virtual void execute()
       {
+        player -> cast_pet() -> expiration = 0;
         if ( ! player -> sleeping ) player -> cast_pet() -> dismiss();
       }
     };
-    new ( sim ) expiration_t( sim, this, duration );
+    expiration = new ( sim ) expiration_t( sim, this, duration );
   }
 
   arise();
@@ -156,6 +159,12 @@ void pet_t::dismiss()
   if ( sim -> log ) log_t::output( sim, "%s dismisses %s", owner -> name(), name() );
 
   owner -> active_pets--;
+
+  if ( expiration )
+  {
+    event_t::cancel( expiration );
+    expiration = 0;
+  }
 
   demise();
 }

engine/sc_sim.cpp
@@ -1054,7 +1054,6 @@ void sim_t::reset()
   expected_time = max_time * ( 1.0 + vary_combat_length * iteration_adjust() );
   id = 0;
   current_time = last_event = 0;
-  iteration_dmg = iteration_heal = 0;
   for ( buff_t* b = buff_list; b; b = b -> next )
   {
     b -> reset();
@@ -1077,6 +1076,8 @@ void sim_t::combat_begin()
   if ( debug ) log_t::output( this, "Combat Begin" );
 
   reset();
+
+  iteration_dmg = iteration_heal = 0;
 
   for ( player_t* t = target_list; t; t = t -> next )
   {
@@ -1160,6 +1161,7 @@ void sim_t::combat_end()
   for ( buff_t* b = buff_list; b; b = b -> next )
   {
     b -> expire();
+    b -> combat_end();
   }
 
   total_dmg.add( iteration_dmg );

engine/sc_stats.cpp
@@ -59,8 +59,6 @@ void stats_t::add_child( stats_t* child )
 void stats_t::reset()
 {
   last_execute = -1;
-  iteration_actual_amount = 0;
-  iteration_total_amount = 0;
 }
 
 // stats_t::add_result ======================================================
@@ -121,6 +119,15 @@ void stats_t::add_tick( double time )
 {
   num_ticks++;
   total_tick_time += time;
+}
+
+// stats_t::combat_begin ========================================================
+
+void stats_t::combat_begin()
+{
+
+  iteration_actual_amount = 0;
+  iteration_total_amount = 0;
 }
 
 // stats_t::combat_end ========================================================

engine/simulationcraft.h
@@ -2590,6 +2590,8 @@ public:
   void init();
   void init_buff_t_();
   virtual void parse_options( va_list vap );
+  virtual void combat_begin();
+  virtual void combat_end();
 
   const char* name() { return name_str.c_str(); }
 
@@ -4348,6 +4350,7 @@ struct pet_t : public player_t
   double summon_time;
   bool summoned;
   pet_type_t pet_type;
+  event_t* expiration;
 
 private:
   void init_pet_t_();
@@ -4433,6 +4436,7 @@ struct stats_t
   void add_result( double act_amount, double tot_amount, int dmg_type, int result );
   void add_tick   ( double time );
   void add_execute( double time );
+  void combat_begin();
   void combat_end();
   void reset();
   void analyze();

commit 11df1b92a7437124d490a5611a47c9024990910d

Player: Remove dangerous assert introduced in r9930 by accident.
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@9938 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_player.cpp
@@ -2812,8 +2812,6 @@ void player_t::combat_end()
   }
   compound_dmg.add( iteration_dmg );
 
-  assert( sim -> current_time > 0  );
-
   dps.add( iteration_fight_length ? iteration_dmg / iteration_fight_length : 0 );
   dpse.add( sim -> current_time ? iteration_dmg / sim -> current_time : 0 );
 