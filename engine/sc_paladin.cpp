Ultis/simulationcraft

commit 182bc0bd196a19ace7f1fe7af223cb9ea1bb0ab8

Paladin:   Add Conviction   Fixes issue 929
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@10083 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_paladin.cpp
@@ -42,6 +42,7 @@ struct paladin_t : public player_t
   buff_t* buffs_ancient_power;
   buff_t* buffs_avenging_wrath;
   buff_t* buffs_censure;
+  buff_t* buffs_conviction;
   buff_t* buffs_daybreak;
   buff_t* buffs_divine_favor;
   buff_t* buffs_divine_plea;
@@ -439,6 +440,16 @@ struct paladin_heal_t : public heal_t
     return heal_t::cost();
   }
 
+  virtual void execute()
+  {
+    paladin_t* p = player -> cast_paladin();
+
+    heal_t::execute();
+
+    if ( result == RESULT_CRIT && direct_dmg > 0 )
+      p -> buffs_conviction -> trigger();
+  }
+
   virtual double haste() SC_CONST
   {
     double h = spell_t::haste();
@@ -456,6 +467,11 @@ struct paladin_heal_t : public heal_t
     paladin_t* p = player -> cast_paladin();
 
     heal_t::player_buff();
+
+    if ( p -> buffs_conviction -> up() )
+    {
+      player_multiplier *= 1.0 + p -> buffs_conviction -> effect2().percent();
+    }
 
     if ( p -> buffs_divine_favor -> up() )
     {
@@ -574,6 +590,9 @@ struct paladin_attack_t : public attack_t
           pa -> procs_wasted_divine_purpose -> occur();
         }
       }
+
+      if ( result == RESULT_CRIT && direct_dmg > 0 )
+        pa -> buffs_conviction -> trigger();
     }
   }
 
@@ -589,6 +608,11 @@ struct paladin_attack_t : public attack_t
     if ( p -> set_bonus.tier13_4pc_melee() && p -> buffs_zealotry -> check() )
     {
       player_multiplier *= 1.12;
+    }
+    
+    if ( p -> buffs_conviction -> up() )
+    {
+      player_multiplier *= 1.0 + p -> buffs_conviction -> effect1().percent();
     }
 
     if ( p -> buffs_divine_shield -> up() )
@@ -622,7 +646,6 @@ struct paladin_attack_t : public attack_t
       p -> buffs_divine_purpose -> expire();
   }
 };
-
 
 // Melee Attack =============================================================
 
@@ -1649,9 +1672,10 @@ struct paladin_spell_t : public spell_t
     spell_t::execute();
     if ( result_is_hit() )
     {
+      paladin_t* pa = player -> cast_paladin();
+
       if ( trigger_dp )
       {
-        paladin_t* pa = player -> cast_paladin();
         bool already_up = pa -> buffs_divine_purpose -> check() != 0;
         bool triggered = pa -> buffs_divine_purpose -> trigger();
         if ( already_up && triggered )
@@ -1659,6 +1683,8 @@ struct paladin_spell_t : public spell_t
           pa -> procs_wasted_divine_purpose -> occur();
         }
       }
+      if ( result == RESULT_CRIT && direct_dmg > 0 )
+        pa -> buffs_conviction -> trigger();
     }
   }
 
@@ -1689,6 +1715,11 @@ struct paladin_spell_t : public spell_t
     if ( p -> set_bonus.tier13_4pc_melee() && p -> buffs_zealotry -> check() )
     {
       player_multiplier *= 1.12;
+    }
+
+    if ( p -> buffs_conviction -> up() )
+    {
+      player_multiplier *= 1.0 + p -> buffs_conviction -> effect1().percent();
     }
 
     if ( p -> buffs_divine_shield -> up() )
@@ -2646,6 +2677,7 @@ void paladin_t::init_buffs()
   buffs_ancient_power          = new buff_t( this, 86700, "ancient_power" );
   buffs_avenging_wrath         = new buff_t( this, 31884, "avenging_wrath",  1, 0 ); // Let the ability handle the CD
   buffs_censure                = new buff_t( this, 31803, "censure" );
+  buffs_conviction             = new buff_t( this, talents.conviction -> effect1().trigger_spell_id(), "conviction" );
   buffs_daybreak               = new buff_t( this, talents.daybreak -> effect_trigger_spell( 1 ), "daybreak", talents.daybreak -> proc_chance() );
   buffs_divine_favor           = new buff_t( this, talents.divine_favor -> spell_id(), "divine_favor", 1.0, 0 ); // Let the ability handle the CD
   buffs_divine_favor -> buff_duration += glyphs.divine_favor -> effect1().seconds();

engine/sc_warrior.cpp
@@ -1350,35 +1350,33 @@ struct bloodthirst_heal_t : public heal_t
     // Add Field Dressing Talent, warrior heal etc.
 
     // Implemented as an actual heal because of spell callbacks ( for Hurricane, etc. )
-
     resource = RESOURCE_NONE;
     background= true;
-
   }
 };
 
 struct bloodthirst_buff_callback_t : public action_callback_t
+{
+  buff_t* buff;
+  bloodthirst_heal_t* bloodthirst_heal;
+
+  bloodthirst_buff_callback_t( warrior_t* p, buff_t* b ) :
+    action_callback_t( p -> sim, p ), buff( b ),
+    bloodthirst_heal( 0 )
+  {
+    bloodthirst_heal = new bloodthirst_heal_t( p );
+  }
+
+  virtual void trigger( action_t* a , void* /* call_data */ )
+  {
+    if ( buff -> check() && a -> weapon && a -> direct_dmg > 0 )
     {
-      buff_t* buff;
-      bloodthirst_heal_t* bloodthirst_heal;
-
-      bloodthirst_buff_callback_t( warrior_t* p, buff_t* b ) :
-        action_callback_t( p -> sim, p ), buff( b )
-      {
-        bloodthirst_heal = new bloodthirst_heal_t( p );
-      }
-      virtual void trigger( action_t* a , void* /* call_data */ )
-      {
-
-        if ( buff -> check() && a -> weapon && a -> direct_dmg > 0 )
-        {
-
-          bloodthirst_heal -> base_dd_min = bloodthirst_heal -> base_dd_max = bloodthirst_heal -> effect2().base_value() / 100000.0 * a -> player -> resource_max[ RESOURCE_HEALTH ];
-          bloodthirst_heal -> execute();
-          buff -> decrement( 1 );
-        }
-      }
-    };
+      bloodthirst_heal -> base_dd_min = bloodthirst_heal -> base_dd_max = bloodthirst_heal -> effect2().base_value() / 100000.0 * a -> player -> resource_max[ RESOURCE_HEALTH ];
+      bloodthirst_heal -> execute();
+      buff -> decrement();
+    }
+  }
+};
 
 
 // Bloodthirst ==============================================================
@@ -1387,7 +1385,6 @@ struct bloodthirst_t : public warrior_attack_t
 {
   bloodthirst_t( warrior_t* p, const std::string& options_str ) :
     warrior_attack_t( "bloodthirst", "Bloodthirst", p )
-
   {
     check_spec( TREE_FURY );
 
@@ -1403,7 +1400,6 @@ struct bloodthirst_t : public warrior_attack_t
     base_multiplier   *= 1.0 + p -> glyphs.bloodthirst -> effect1().percent()
                              + p -> set_bonus.tier11_2pc_melee() * 0.05;
     base_crit         += p -> talents.cruelty -> effect1().percent();
-
   }
 
   virtual void execute()
@@ -3453,7 +3449,6 @@ void warrior_t::init_buffs()
 
   player_t::init_buffs();
 
-  // buff_t( sim, name, max_stack, duration, cooldown, proc_chance, quiet )
   buffs_bastion_of_defense        = new buff_t( this, "bastion_of_defense",        1, 12.0,   0, talents.bastion_of_defense -> proc_chance() );
   buffs_battle_stance             = new buff_t( this, 21156, "battle_stance" );
   buffs_battle_trance             = new buff_t( this, "battle_trance",             1, 15.0,   0, talents.battle_trance -> proc_chance() );
@@ -3501,9 +3496,6 @@ void warrior_t::init_buffs()
   buffs_tier12_2pc_melee          = new buff_t( this, "tier12_2pc_melee",          1, duration,   0, set_bonus.tier12_2pc_melee() );
 
   register_direct_damage_callback( SCHOOL_ATTACK_MASK, new bloodthirst_buff_callback_t( this, buffs_bloodthirst ) );
-
-
-  // buff_t( sim, name, max_stack, duration, cooldown, proc_chance, quiet )
 }
 
 // warrior_t::init_values ====================================================

commit 1f97f8549f9d4528ace1bca16edc5c3a489c2af7

Paladin:   Only proc conviction when we have conviction   Fixes a crash introduced in r10083
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@10087 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_paladin.cpp
@@ -2711,7 +2711,7 @@ void paladin_t::init_buffs()
   buffs_ancient_power          = new buff_t( this, 86700, "ancient_power" );
   buffs_avenging_wrath         = new buff_t( this, 31884, "avenging_wrath",  1, 0 ); // Let the ability handle the CD
   buffs_censure                = new buff_t( this, 31803, "censure" );
-  buffs_conviction             = new buff_t( this, talents.conviction -> effect1().trigger_spell_id(), "conviction" );
+  buffs_conviction             = new buff_t( this, talents.conviction -> effect1().trigger_spell_id(), "conviction", talents.conviction -> rank() );
   buffs_daybreak               = new buff_t( this, talents.daybreak -> effect_trigger_spell( 1 ), "daybreak", talents.daybreak -> proc_chance() );
   buffs_divine_favor           = new buff_t( this, talents.divine_favor -> spell_id(), "divine_favor", 1.0, 0 ); // Let the ability handle the CD
   buffs_divine_favor -> buff_duration += glyphs.divine_favor -> effect1().seconds();